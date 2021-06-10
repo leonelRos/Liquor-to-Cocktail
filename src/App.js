99b7aba866185f2700fe8680d27ade489803b43f
leonelros <rleonel426@gmail.com>
2021-06-09 22:16:46 -0700

    login and logout added

--- src/App.js
+++ src/App.js
@@ -9,32 +9,35 @@ import CocktailDetail from "./pages/CocktailDetail";
 //import components
 import Navbar from "./components/Navbar";
 import NewCocktail from "./pages/NewCocktail";
+import Auth0ProviderWithHistory from "./Auth0Provider";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/cocktail/:id">
-          <SingleCocktail />
-        </Route>
-        <Route path="/newCocktails/:id">
-          <CocktailDetail />
-        </Route>
-        <Route path="/newCocktail">
-          <NewCocktail />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
+      <Auth0ProviderWithHistory>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/cocktail/:id">
+            <SingleCocktail />
+          </Route>
+          <Route path="/newCocktails/:id">
+            <CocktailDetail />
+          </Route>
+          <Route path="/newCocktail">
+            <NewCocktail />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Auth0ProviderWithHistory>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import AuthenticationButton from "../components/authzero/AuthenticationButton";
+// import Login from "./authzero/Login";
+// import LogoutButton from "./authzero/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 import cocktails_logo from "../logo/cocktails_logo.png";
+import Login from "./authzero/Login";
+import LogoutButton from "./authzero/LogoutButton";
 /*src={logo of the project} */
 export default function Navbar() {
+  const { user, isLoading } = useAuth0();
   return (
     <nav className="navbar">
       <div className="nav-content">
         <Link to="/">
           <img src={cocktails_logo} alt="logo" className="logo" />
         </Link>
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/newCocktail">new cocktail</Link>
-          </li>
+        {!isLoading && !user && <Login />}
+        {!isLoading && user && (
+          <React.Fragment>
+            <ul className="nav-links">
+              <LogoutButton />
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/newCocktail">new cocktail</Link>
+              </li>
 
-          <li>
-            <Link to="/about">spirit/wine</Link>
-          </li>
-          <li>
-            <Link to="/about">News</Link>
-          </li>
-        </ul>
+              <li>
+                <Link to="/about">spirit/wine</Link>
+              </li>
+              <li>
+                <Link to="/about">News</Link>
+              </li>
+            </ul>
+          </React.Fragment>
+        )}
       </div>
     </nav>
   );
